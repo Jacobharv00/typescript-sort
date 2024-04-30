03f839e1bad18f8f21afa4b147988c5fb5182f33
Jacob Harvey <jharvey@mahalobanking.com>
2024-04-30 08:39:48 -0600

    chore: remove unused interface from Sorter.ts

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,9 +1,3 @@
-interface Sortable {
-  length: number;
-  compare: (leftIndex: number, rightIndex: number) => boolean;
-  swap: (leftIndex: number, rightIndex: number) => void;
-}
-
 /**
  * Abstract class for sorting.
  *
